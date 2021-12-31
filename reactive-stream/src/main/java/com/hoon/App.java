af0f6c841c94dea9bfb8ae4340c1c3732bf869bd
byeonguhun <dksekfldks65@naver.com>
2021-12-31 19:23:15 +0900

    reactive programming 예제

---
+++ reactive-stream/src/main/java/com/hoon/App.java
@@ -0,0 +1,12 @@
+package com.hoon;
+
+public class App {
+
+    public static void main(String[] args) {
+        MyPub pub = new MyPub();  // 신문사 생성
+        MySub sub = new MySub();  // 구독자 생성
+
+        pub.subscribe(sub); // 구독 시작
+    }
+
+}
---
+++ reactive-stream/src/main/java/com/hoon/MySubscription.java
@@ -0,0 +1,38 @@
+package com.hoon;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.Iterator;
+
+/**
+ * 구독 정보(구독자, 어떤 데이터를 구독할지)
+ */
+public class MySubscription implements Subscription {
+
+    private Subscriber subscriber;
+    private Iterator<Integer> it;
+
+    public MySubscription(Subscriber subscriber, Iterable<Integer> its) {
+        this.subscriber = subscriber;
+        this.it = its.iterator();
+    }
+
+    @Override
+    public void request(long n) {
+        while( n > 0 ) {
+            if(it.hasNext()) {
+                subscriber.onNext(it.next());
+            } else {
+                subscriber.onComplete();
+                break;
+            }
+            n--;
+        }
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+}
