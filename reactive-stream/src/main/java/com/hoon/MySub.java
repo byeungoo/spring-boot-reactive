76af5516fec04079a17f11c97eb6d2a5c0c04f23
byeungoo <dksekfldks65@naver.com>
2021-12-31 20:57:27 +0900

    reactive stream 예제 코드 작성 완료

--- reactive-stream/src/main/java/com/hoon/MySub.java
+++ reactive-stream/src/main/java/com/hoon/MySub.java
@@ -6,18 +6,24 @@ import org.reactivestreams.Subscription;
 public class MySub implements Subscriber<Integer> {
 
     private Subscription subscription;
+    private int bufferSize = 3;
 
     @Override
     public void onSubscribe(Subscription subscription) {
         System.out.println("구독자 : 구독 정보 잘 받았어");
         this.subscription = subscription;
-        subscription.request(1);    // 신문 한개씩 매일 매일 줘!! (백프레셔) 소비자가 한번에 처리할 수 있는 개수를 요청
+        subscription.request(bufferSize);    // 신문 한개씩 매일 매일 줘!! (백프레셔) 소비자가 한번에 처리할 수 있는 개수를 요청
     }
 
     @Override
     public void onNext(Integer integer) {
         System.out.println("onNext() : : " + integer);
-        subscription.request(1);
+        bufferSize--;
+        if(bufferSize == 0) {
+            System.out.println("하루 지남");
+            bufferSize = 3;
+            subscription.request(bufferSize);
+        }
     }
 
     @Override
